fddd5b4bdcd3a194f274bfe5abcd1f85c5ec370f
Ingeborg Wilsher <ingeborgwilsher@Ingeborgs-MacBook-Air.local>
2020-10-21 17:27:46 +0200

    Updates

---
+++ precache-manifest.3c5098b7960cecb2bfd512010abb816d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc17d2f14a12d394ec937bbf03a2f974",
+    "url": "/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "e7461307098e4fea83a7",
+    "url": "/webkurs2020-CICD-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7461307098e4fea83a7",
+    "url": "/webkurs2020-CICD-React/static/js/main.1f1fccc1.chunk.js"
+  },
+  {
+    "revision": "580e99ae8bc13b712167",
+    "url": "/webkurs2020-CICD-React/static/js/runtime-main.89e14c0b.js"
+  }
+]);
